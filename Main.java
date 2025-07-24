43d1432bff3b7a9e4bb528340495818208c7d726
Goutami <goutamideshpande394@gmail.com>
2025-07-24 19:35:00 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+import java.util.List;
+
+import java.util.ArrayList;
+
+import java.util.Collections;
+ 
+public class Main {
+
+    public static void main(String[] args) {
+
+        Integer[] arr1 = {3, 7, 1};
+
+        Integer[] arr2 = {9, 4, 6};
+
+        List<Integer> mergedList = new ArrayList<>();
+
+        mergedList.addAll(Arrays.asList(arr1));
+        mergedList.addAll(Arrays.asList(arr2));
+        mergedList.sort(Collections.reverseOrder());
+        System.out.println(mergedList);
+
+    }
+
+}
+ 
